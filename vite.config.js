fce11d9e3381a0022534a83707eaf5553c0270af
gaurav060404 <gs762282@gmail.com>
2025-06-28 20:12:20 +0530

    Remove server allowedHosts configuration from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['assumes-press-salmon-clearance.trycloudflare.com'],
-  },
+  base : process.env.VITE_URL || "/DocDynamoFrontend"
 })
